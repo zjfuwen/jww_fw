657a19427a8609fec2529a7be7b1c543ebcf4a8f
wanyong <7742533@qq.com>
2017-11-27 16:22:33 +0800

    优化fastjson配置

---
+++ jww-common/jww-common-web/src/main/java/com/jww/common/web/configuration/FastJsonConfig.java
@@ -0,0 +1,77 @@
+package com.jww.common.web.configuration;
+
+import com.alibaba.fastjson.serializer.SerializeConfig;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.serializer.ToStringSerializer;
+import com.alibaba.fastjson.serializer.ValueFilter;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter4;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+
+import java.math.BigInteger;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * fastjson配置
+ *
+ * @author wanyong
+ * @date 2017/11/27 16:19
+ */
+@Slf4j
+@Configuration
+@ConditionalOnClass(com.alibaba.fastjson.JSON.class)
+@ConditionalOnMissingBean(FastJsonHttpMessageConverter4.class)
+@ConditionalOnWebApplication
+public class FastJsonConfig {
+    @Bean
+    public FastJsonHttpMessageConverter4 fastJsonHttpMessageConverter() {
+        FastJsonHttpMessageConverter4 converter = new FastJsonHttpMessageConverter4();
+        converter.setFastJsonConfig(fastjsonConfig());
+        converter.setSupportedMediaTypes(getSupportedMediaType());
+        return converter;
+    }
+
+    public com.alibaba.fastjson.support.config.FastJsonConfig fastjsonConfig() {
+        com.alibaba.fastjson.support.config.FastJsonConfig fastJsonConfig = new com.alibaba.fastjson.support.config.FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(
+                SerializerFeature.PrettyFormat,
+                SerializerFeature.WriteMapNullValue
+        );
+        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
+        ValueFilter valueFilter = new ValueFilter() {
+            @Override
+            public Object process(Object o, String s, Object o1) {
+                if (null == o1) {
+                    o1 = "";
+                }
+                return o1;
+            }
+        };
+        fastJsonConfig.setCharset(Charset.forName("utf-8"));
+        fastJsonConfig.setSerializeFilters(valueFilter);
+
+        //解决Long转json精度丢失的问题
+        SerializeConfig serializeConfig = SerializeConfig.globalInstance;
+        serializeConfig.put(BigInteger.class, ToStringSerializer.instance);
+        serializeConfig.put(Long.class, ToStringSerializer.instance);
+        serializeConfig.put(Long.TYPE, ToStringSerializer.instance);
+        fastJsonConfig.setSerializeConfig(serializeConfig);
+        return fastJsonConfig;
+    }
+
+    /**
+     * 支持的mediaType类型
+     */
+    public List<MediaType> getSupportedMediaType() {
+        ArrayList<MediaType> mediaTypes = new ArrayList<>();
+        mediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        return mediaTypes;
+    }
+}
