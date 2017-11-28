87426dd31fb1a47d7e224b81dfbf51b92fca91d6
shadj <jiangxinde2000@sina.com>
2017-11-29 00:59:53 +0800

    增加XSSFilter

---
+++ jww-common/jww-common-web/src/main/java/com/jww/common/web/filter/XssFilter.java
@@ -0,0 +1,57 @@
+package com.jww.common.web.filter;
+
+import com.xiaoleilu.hutool.util.StrUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * 防XSS漏洞过滤器
+ *
+ * @author shadj
+ * @date 2017/11/28 22:21
+ */
+@Slf4j
+@Order(1)
+@WebFilter(filterName = "xssFilter",urlPatterns="/*")
+public class XssFilter implements Filter {
+
+    private String[] excludePaths = null;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("init XssFilter");
+        String excludePath = filterConfig.getInitParameter("excludePaths");
+        if(StrUtil.isNotBlank(excludePath)) {
+            excludePaths = excludePath.split(",");
+        }
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        log.info("XssFilter.doFilter ...");
+        if(excludePaths != null){
+            HttpServletRequest request = (HttpServletRequest)servletRequest;
+            String uri = request.getRequestURI();
+            // 排除部分URL不做过滤。
+            for (String str : excludePaths) {
+                if (uri.indexOf(str) >= 0) {
+                    log.info("该URL不作校验：" + uri);
+                    filterChain.doFilter(servletRequest, servletResponse);
+                    return;
+                }
+            }
+        }
+        XssHttpRequestWrapper xssHttpServletRequest = new XssHttpRequestWrapper((HttpServletRequest) servletRequest);
+        filterChain.doFilter(xssHttpServletRequest,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
--- jww-ump/jww-ump-server/src/main/java/com/jww/ump/server/ServerApplication.java
+++ jww-ump/jww-ump-server/src/main/java/com/jww/ump/server/ServerApplication.java
@@ -2,6 +2,7 @@ package com.jww.ump.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
@@ -12,9 +13,23 @@ import org.springframework.context.annotation.ImportResource;
  */
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.jww.ump.server", "com.jww.common.web", "com.jww.common.log.web"})
+@ServletComponentScan("com.jww.common.web.filter")
 @ImportResource(value = {"classpath:dubbo/consumers.xml"})
 public class ServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
     }
+
+/*    @Bean
+    public FilterRegistrationBean webAppForIndexFilterRegistrationBean() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setName("webAppForIndexFilter");
+        XssFilter xssFilter = new XssFilter();
+        registrationBean.setFilter(xssFilter);
+        registrationBean.setOrder(1);
+        List<String> urlList = new ArrayList<String>();
+        urlList.add("/*");
+        registrationBean.setUrlPatterns(urlList);
+        return registrationBean;
+    }*/
 }
