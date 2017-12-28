95e6a498f24a5bfe6759ddf6904de17319e88b27
rickyw <cs_wyx@foxmail.com>
2017-12-28 22:50:42 +0800

    1.更改日志注解名字 4.更改注解参数为枚举类型

--- jww-common/jww-common-core/src/main/java/com/jww/common/core/Constants.java
+++ jww-common/jww-common-core/src/main/java/com/jww/common/core/Constants.java
@@ -45,30 +45,6 @@ public final class Constants {
      * CAPTCHA缓存命名空间
      */
     public static final String CAPTCHA_CACHE_NAMESPACE = CACHE_NAMESPACE + "captcha:";
-    /**
-     * 日志操作类型-新增
-     */
-    public static final int LOG_OPERATION_TYPE_INSERT = 1;
-    /**
-     * 日志操作类型-修改
-     */
-    public static final int LOG_OPERATION_TYPE_MODIFY = 2;
-    /**
-     * 日志操作类型-删除
-     */
-    public static final int LOG_OPERATION_TYPE_DELETE = 3;
-    /**
-     * 日志操作类型-登录
-     */
-    public static final int LOG_OPERATION_TYPE_LOGIN = 4;
-    /**
-     * 日志操作类型-查询
-     */
-    public static final int LOG_OPERATION_TYPE_QUERY = 0;
-    /**
-     * 日志操作类型-其他
-     */
-    public static final int LOG_OPERATION_TYPE_UNKONW = 9;
 
     /**
      * 缓存键值
@@ -171,23 +147,27 @@ public final class Constants {
     /**
      * 日志操作类型枚举
      */
-    public enum LogOperationTypeEnum {
-        /**
-         * 查询
-         */
-        QUERY(0),
-        ADD(1),
-        MODIFY(2),
-        DELETE(3);
-
+    public enum LogOptEnum {
+        QUERY(0,"查询"),
+        ADD(1,"新增"),
+        MODIFY(2,"修改"),
+        DELETE(3,"删除"),
+        LOGIN(4,"登录"),
+        UNKNOW(9,"未知");
         private final int value;
+        private final String message;
 
-        LogOperationTypeEnum(int value) {
+        LogOptEnum(int value, String message) {
             this.value = value;
+            this.message = message;
         }
 
         public int value() {
             return this.value;
+        }
+
+        public String getMessage() {
+            return this.message;
         }
 
     }
