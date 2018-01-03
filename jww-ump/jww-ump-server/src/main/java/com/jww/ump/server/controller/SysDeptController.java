87efec3b5fea265d231b51e1935c6c9f60976378
wanhaha555 <wanhaha000>
2018-01-03 13:10:41 +0800

    增加swagger2配置

--- jww-ump/jww-ump-server/src/main/java/com/jww/ump/server/controller/SysDeptController.java
+++ jww-ump/jww-ump-server/src/main/java/com/jww/ump/server/controller/SysDeptController.java
@@ -13,7 +13,6 @@ import com.xiaoleilu.hutool.lang.Assert;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -25,12 +24,11 @@ import java.util.List;
 /**
  * 部门管理控制器
  *
- * @author: Ricky Wang
- * @Date: 17/12/1 11:23:17
+ * @author Ricky Wang
+ * @date 17/12/1 11:23:17
  */
 @RestController
 @RequestMapping("/dept")
-@Slf4j
 @Api(value = "部门管理", description = "部门管理")
 public class SysDeptController extends BaseController {
 
@@ -40,7 +38,7 @@ public class SysDeptController extends BaseController {
     /**
      * 根据部门ID查询
      *
-     * @param id
+     * @param id 部门ID
      * @return ResultModel<SysDeptModel>
      * @author RickyWang
      * @date 2017-12-05 13:35
@@ -49,7 +47,7 @@ public class SysDeptController extends BaseController {
     @ApiImplicitParam(name = "id", value = "部门ID", required = true, dataType = "Long")
     @GetMapping("/query/{id}")
     @RequiresPermissions("sys:dept:read")
-    public ResultModel<SysDeptModel> query(@PathVariable Long id) {
+    public ResultModel query(@PathVariable Long id) {
         Assert.notNull(id);
         SysDeptModel sysDeptModel = sysDeptService.queryOne(id);
         return ResultUtil.ok(sysDeptModel);
@@ -58,11 +56,12 @@ public class SysDeptController extends BaseController {
     /**
      * 查询部门分页方法
      *
-     * @param pageModel
+     * @param pageModel 分页实体
      * @return com.jww.common.web.model.ResultModel
      * @author RickyWang
      * @date 17/12/25 21:28:13
      */
+    @ApiOperation(value = "分页查询部门列表", notes = "根据分页参数查询部门列表")
     @PostMapping("/queryListPage")
     @RequiresPermissions("sys:dept:read")
     public ResultModel queryListPage(@RequestBody PageModel<SysDeptModel> pageModel) {
@@ -73,16 +72,16 @@ public class SysDeptController extends BaseController {
     /**
      * 新增部门方法
      *
-     * @param sysDeptModel
+     * @param sysDeptModel 部门实体
      * @return com.jww.common.web.model.ResultModel
      * @author RickyWang
      * @date 17/12/25 21:28:41
      */
+    @ApiOperation(value = "新增部门", notes = "根据部门实体新增部门")
     @PostMapping("/add")
     @RequiresPermissions("sys:dept:add")
     @SysLogOpt(module = "部门管理", value = "部门新增", operationType = Constants.LogOptEnum.ADD)
     public ResultModel add(@Valid @RequestBody SysDeptModel sysDeptModel) {
-        log.info("DeptController->add: SysDeptModel={}", sysDeptModel);
         if (sysDeptModel != null) {
             sysDeptModel.setCreateBy(this.getCurrUser());
             sysDeptModel.setUpdateBy(this.getCurrUser());
@@ -93,16 +92,16 @@ public class SysDeptController extends BaseController {
     /**
      * 修改部门方法
      *
-     * @param sysDeptModel
+     * @param sysDeptModel 部门实体
      * @return com.jww.common.web.model.ResultModel
      * @author RickyWang
      * @date 17/12/25 21:29:09
      */
+    @ApiOperation(value = "修改部门", notes = "根据主键ID修改部门")
     @PutMapping("/modify")
     @RequiresPermissions("sys:dept:update")
     @SysLogOpt(module = "部门管理", value = "部门修改", operationType = Constants.LogOptEnum.MODIFY)
     public ResultModel modify(@RequestBody SysDeptModel sysDeptModel) {
-        log.info("DeptController->mod: SysDeptModel={}", sysDeptModel);
         sysDeptModel.setUpdateBy(this.getCurrUser());
         sysDeptModel.setUpdateTime(new Date());
         sysDeptService.modifyById(sysDeptModel);
@@ -112,11 +111,12 @@ public class SysDeptController extends BaseController {
     /**
      * 批量删除部门方法
      *
-     * @param ids
+     * @param ids 部门ID集合
      * @return com.jww.common.web.model.ResultModel
      * @author RickyWang
      * @date 17/12/25 21:29:23
      */
+    @ApiOperation(value = "批量删除部门", notes = "根据主键ID集合批量删除部门")
     @DeleteMapping("/delBatchByIds")
     @RequiresPermissions("sys:dept:delete")
     @SysLogOpt(module = "部门管理", value = "部门批量删除", operationType = Constants.LogOptEnum.DELETE)
@@ -128,11 +128,12 @@ public class SysDeptController extends BaseController {
     /**
      * 根据部门id过滤查询部门树方法
      *
-     * @param id
+     * @param id 部门ID
      * @return com.jww.common.web.model.ResultModel
      * @author RickyWang
      * @date 17/12/25 21:29:52
      */
+    @ApiOperation(value = "查询部门树", notes = "根据部门ID查询部门树结构数据")
     @GetMapping("/queryTree/{id}")
     @RequiresPermissions("sys:dept:read")
     public ResultModel queryTree(@PathVariable(value = "id", required = false) Long id) {
@@ -143,11 +144,11 @@ public class SysDeptController extends BaseController {
     /**
      * 查询部门树方法
      *
-     * @param
      * @return com.jww.common.web.model.ResultModel
      * @author RickyWang
      * @date 17/12/25 21:30:28
      */
+    @ApiOperation(value = "查询部门树", notes = "查询全部部门树结构数据")
     @GetMapping("/queryTree")
     @RequiresPermissions("sys:dept:read")
     public ResultModel queryTree() {
@@ -158,11 +159,12 @@ public class SysDeptController extends BaseController {
     /**
      * 删除部门方法
      *
-     * @param id
+     * @param id 部门ID
      * @return com.jww.common.web.model.ResultModel
      * @author RickyWang
      * @date 17/12/25 21:30:45
      */
+    @ApiOperation(value = "删除部门", notes = "根据部门ID删除部门")
     @DeleteMapping("/delDept")
     @RequiresPermissions("sys:dept:delete")
     @SysLogOpt(module = "部门管理", value = "部门删除", operationType = Constants.LogOptEnum.DELETE)
