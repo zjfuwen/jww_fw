b11f87fc2f14b423dcfaf05740a5da71a58a7611
aimee <78441196@qq.com>
2018-01-05 12:34:45 +0800

    1. 代码优化

--- jww-ump/jww-ump-server/src/main/java/com/jww/ump/server/ServerApplication.java
+++ jww-ump/jww-ump-server/src/main/java/com/jww/ump/server/ServerApplication.java
@@ -1,5 +1,6 @@
 package com.jww.ump.server;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.ImportResource;
  * @author wanyong
  * @date 2017/11/17 00:34
  */
+@Slf4j
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.jww.ump.server", "com.jww.common.web", "com.jww.common.dsession", "com.jww.common.log.web", "com.jww.common.redis"})
 @ServletComponentScan({"com.jww.common.web.filter", "com.jww.ump.server.druid"})
@@ -19,6 +21,7 @@ import org.springframework.context.annotation.ImportResource;
 public class ServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
+        log.info("========== ServerApplication启动成功 ==========");
     }
 
 /*    @Bean
